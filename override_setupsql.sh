3a5d97c73c8c0fb71634b0f7af41883c7850a248
Andreas Schmid <andreas.schmid@bd.so.ch>
2019-08-19 07:11:43 +0200

    Fix grants for PG_WRITE_USER

--- override_setupsql.sh
+++ override_setupsql.sh
@@ -5,8 +5,9 @@ curl https://raw.githubusercontent.com/sogis/oereb-db/master/sql/setup_original.
 curl https://raw.githubusercontent.com/sogis/oereb-db/master/sql/set_role.sql > /pgconf/set_role.sql
 curl https://raw.githubusercontent.com/sogis/oereb-db/master/sql/transfer_arp_npl_oereb_gdi.sql > /pgconf/transfer_arp_npl_oereb_gdi.sql
 cat /pgconf/setup_original.sql /pgconf/set_role.sql /pgconf/transfer_arp_npl_oereb_gdi.sql > /pgconf/setup.sql
-sed -i s/:PG_WRITE_USER/${PG_WRITE_USER}/g /pgconf/setup.sql
+# Hack/unnecessary but required command (PG_WRITE_USER does not exist yet):
+sed -i s/:PG_WRITE_USER/${PG_USER}/g /pgconf/setup.sql
 
-echo "GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA arp_npl_oereb TO ${PG_WRITE_USER}; GRANT USAGE ON ALL SEQUENCES IN SCHEMA arp_npl_oereb TO ${PG_WRITE_USER};" > /pgconf/grants.sql
+echo "CREATE USER :PG_WRITE_USER WITH PASSWORD :'PG_WRITE_PASSWORD'; GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA arp_npl_oereb TO :PG_WRITE_USER; GRANT USAGE ON ALL SEQUENCES IN SCHEMA arp_npl_oereb TO :PG_WRITE_USER;" > /pgconf/grants.sql
 
 exec "$@"
