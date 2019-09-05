5a0acbf2fb9173cc976a149e2e77c5a8e77cbedb
Andreas Schmid <andreas.schmid@bd.so.ch>
2019-09-05 17:53:01 +0200

    Use original t_id values

--- oereb_av/select_gebaeudeadressen_lokalisationsname.sql
+++ oereb_av/select_gebaeudeadressen_lokalisationsname.sql
@@ -1,3 +1,3 @@
-SELECT t_id + 999999999 AS t_id, -1::int8 AS t_basket, 'dm01vch24lv95dgemeindegrenzen_lsnachfuehrung'::varchar(60) AS t_type, NULL::varchar(200) AS t_ili_tid,
-  atext, kurztext, indextext, sprache, benannte + 999999999 AS benannte
+SELECT t_id AS t_id, -1::int8 AS t_basket, 'dm01vch24lv95dgemeindegrenzen_lsnachfuehrung'::varchar(60) AS t_type, NULL::varchar(200) AS t_ili_tid,
+  atext, kurztext, indextext, sprache, benannte AS benannte
 FROM agi_dm01avso24.gebaeudeadressen_lokalisationsname;
--- oereb_av/select_gemeindegrenzen_gemeindegrenze.sql
+++ oereb_av/select_gemeindegrenzen_gemeindegrenze.sql
@@ -1,3 +1,3 @@
-SELECT t_id + 999999999 AS t_id, -1::int8 AS t_basket, 'dm01vch24lv95dgemeindegrenzen_gemeindegrenze'::varchar(60) AS t_type, NULL::varchar(200) AS t_ili_tid,
-  ST_CurveToLine(geometrie)::geometry(POLYGON,2056) AS geometrie, entstehung + 999999999 AS entstehung, gemeindegrenze_von + 999999999 AS gemeindegrenze_von
+SELECT t_id AS t_id, -1::int8 AS t_basket, 'dm01vch24lv95dgemeindegrenzen_gemeindegrenze'::varchar(60) AS t_type, NULL::varchar(200) AS t_ili_tid,
+  ST_CurveToLine(geometrie)::geometry(POLYGON,2056) AS geometrie, entstehung AS entstehung, gemeindegrenze_von AS gemeindegrenze_von
 FROM agi_dm01avso24.gemeindegrenzen_gemeindegrenze;
