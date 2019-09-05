bd60daa64c87e7716a95cd48670be201daf1276f
Andreas Schmid <andreas.schmid@bd.so.ch>
2019-09-05 17:47:13 +0200

    Extend insert query

--- oereb_av/create_basket.sql
+++ oereb_av/create_basket.sql
@@ -1,6 +1,6 @@
 INSERT INTO
-  ${dbSchema}.t_ili2db_basket
+  ${dbSchema}.t_ili2db_basket (t_id, dataset, topic, t_ili_tid, attachmentkey, domains)
 VALUES
-  (-1, null, 'AV-Daten importiert per Db2Db-Step', null, '-')
+  (-1, null, 'AV-Daten importiert per Db2Db-Step', null, '-', null)
 ON CONFLICT DO NOTHING
 ;
