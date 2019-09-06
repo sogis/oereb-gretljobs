7e955d883e4e8e1f09c1ebda05219646d43eea23
Andreas Neumann <a.neumann@carto.net>
2019-09-06 15:33:27 +0200

    [oereb_gbkreise] adding import of Grundbuchkreise

---
+++ oereb_gbkreise/create_basket.sql
@@ -0,0 +1,6 @@
+ INSERT INTO
+  ${dbSchema}.t_ili2db_basket (t_id, dataset, topic, t_ili_tid, attachmentkey, domains)
+VALUES
+  (-3, null, 'GBKreis-Daten importiert per Db2Db-Step', null, '-', null)
+ON CONFLICT DO NOTHING
+;
