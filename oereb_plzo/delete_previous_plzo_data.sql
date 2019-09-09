a17c87ee4661761674e32a40279adcc1de1df0f6
Andreas Neumann <a.neumann@carto.net>
2019-09-09 11:37:58 +0200

    [oereb_plzo] Deleting previous records before transfer

---
+++ oereb_plzo/delete_previous_plzo_data.sql
@@ -0,0 +1,8 @@
+-- PLZ
+DELETE FROM ${dbSchema}.plzoch1lv95dplzortschaft_plz6;
+DELETE FROM ${dbSchema}.plzoch1lv95dplzortschaft_plz6nachfuehrung;
+-- Ortschaft
+DELETE FROM ${dbSchema}.plzoch1lv95dplzortschaft_ortschaftsname;
+DELETE FROM ${dbSchema}.plzoch1lv95dplzortschaft_ortschaft;
+DELETE FROM ${dbSchema}.plzoch1lv95dplzortschaft_ortschaftsverbund;
+DELETE FROM ${dbSchema}.plzoch1lv95dplzortschaft_osnachfuehrung;
