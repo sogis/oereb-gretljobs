8663a8f097ff96f8b4b178420c138797eadd9a6b
Andreas Schmid <andreas.schmid@bd.so.ch>
2019-09-10 11:56:32 +0200

    Refresh materialized views

---
+++ refresh_oereb_landuseplans_materialized_views.sql
@@ -0,0 +1,6 @@
+REFRESH MATERIALIZED VIEW ${dbSchema}.vw_oerebwms_nutzungsplanung_baulinien_linie WITH DATA;
+REFRESH MATERIALIZED VIEW ${dbSchema}.vw_oerebwms_nutzungsplanung_grundnutzung_flaeche WITH DATA;
+REFRESH MATERIALIZED VIEW ${dbSchema}.vw_oerebwms_nutzungsplanung_sondernutzungsplaene_flaeche WITH DATA;
+REFRESH MATERIALIZED VIEW ${dbSchema}.vw_oerebwms_nutzungsplanung_ueberlagernd_flaeche WITH DATA;
+REFRESH MATERIALIZED VIEW ${dbSchema}.vw_oerebwms_nutzungsplanung_ueberlagernd_linie WITH DATA;
+REFRESH MATERIALIZED VIEW ${dbSchema}.vw_oerebwms_nutzungsplanung_ueberlagernd_punkt WITH DATA;
