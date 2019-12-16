86dfda3407a49128469c84eec33efe696f20790c
Andreas Neumann <a.neumann@carto.net>
2019-12-16 18:09:08 +0100

    get rid of old unnecessary sql files

--- oereb_annex/synchronize_dataset_and_baskets_stage-live.sql
+++
@@ -1,9 +0,0 @@
--- datasets
-INSERT INTO live.t_ili2db_dataset
-SELECT t_id, datasetname FROM stage.t_ili2db_dataset 
-  WHERE t_id NOT IN (SELECT t_id FROM live.t_ili2db_dataset);
--- baskets
-INSERT INTO live.t_ili2db_basket
-SELECT t_id, dataset, topic, t_ili_tid, attachmentkey, domains
-  FROM stage.t_ili2db_basket
- WHERE t_id NOT IN (SELECT t_id FROM live.t_ili2db_basket)
