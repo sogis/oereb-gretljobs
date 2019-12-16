70d1e7d9319d292db772e67c0eb81d0fa054da6c
Andreas Neumann <a.neumann@carto.net>
2019-12-16 17:40:23 +0100

    incl cleanup of old data

---
+++ oereb_annex/deleteOldLiveSchema.sql
@@ -0,0 +1,11 @@
+--delete everything from live.in the right order
+DELETE FROM live.oerb_xtnx_v1_0annex_logo;
+DELETE FROM live.oerb_xtnx_v1_0annex_glossary;
+DELETE FROM live.oerb_xtnx_v1_0annex_exclusionofliability;
+DELETE FROM live.oerb_xtnx_v1_0annex_generalinformation;
+DELETE FROM live.oerb_xtnx_v1_0annex_thematxt;
+DELETE FROM live.oereb_extractannex_v1_0_code_;
+DELETE FROM live.oerb_xtnx_v1_0annex_municipalitywithplrc;
+DELETE FROM live.oerb_xtnx_v1_0annex_office;
+DELETE FROM live.oerb_xtnx_v1_0annex_maplayering;
+DELETE FROM live.oerb_xtnx_v1_0annex_basedata;
