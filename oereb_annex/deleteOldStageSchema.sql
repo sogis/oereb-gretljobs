2088b682b1b144db8081df960d6c47eb453d3294
Andreas Neumann <a.neumann@carto.net>
2019-12-16 14:29:27 +0100

    next try ...

---
+++ oereb_annex/deleteOldStageSchema.sql
@@ -0,0 +1,11 @@
+--delete everything from stage in the right order
+DELETE FROM stage.oerb_xtnx_v1_0annex_logo;
+DELETE FROM stage.oerb_xtnx_v1_0annex_glossary;
+DELETE FROM stage.oerb_xtnx_v1_0annex_exclusionofliability;
+DELETE FROM stage.oerb_xtnx_v1_0annex_generalinformation;
+DELETE FROM stage.oerb_xtnx_v1_0annex_thematxt;
+DELETE FROM stage.oereb_extractannex_v1_0_code_;
+DELETE FROM stage.oerb_xtnx_v1_0annex_municipalitywithplrc;
+DELETE FROM stage.oerb_xtnx_v1_0annex_office;
+DELETE FROM stage.oerb_xtnx_v1_0annex_maplayering;
+DELETE FROM stage.oerb_xtnx_v1_0annex_basedata;
