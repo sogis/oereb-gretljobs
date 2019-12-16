5b0e08536de2a6a8c388b07070b000308f791c5a
Andreas Neumann <a.neumann@carto.net>
2019-12-16 17:59:50 +0100

    fix order of deletes (stage)

--- oereb_annex/deleteOldStageSchema.sql
+++ oereb_annex/deleteOldStageSchema.sql
@@ -1,14 +1,14 @@
+--delete old baskets
+DELETE FROM stage.t_ili2db_basket WHERE attachmentkey LIKE 'ch.so.agi.OeREB_extractAnnex-AktiveGemeinden.xtf-%';
+DELETE FROM stage.t_ili2db_basket WHERE attachmentkey LIKE 'ch.so.agi.OeREB_extractAnnex-KatatasterAmt.xtf-%';
+DELETE FROM stage.t_ili2db_basket WHERE attachmentkey LIKE 'ch.so.agi.OeREB_extractAnnex-Stammdaten.xtf-%';
+--DELETE FROM stage.t_ili2db_basket WHERE attachmentkey LIKE 'ch.so.agi.OeREB_extractAnnex.xtf-%'
 -- delete old datasets
 DELETE FROM stage.t_ili2db_dataset WHERE datasetname LIKE 'ch.so.agi.OeREB_extractAnnex.xtf-%';
 DELETE FROM stage.t_ili2db_dataset WHERE datasetname LIKE 'ch.so.agi.OeREB_extractAnnex_%';
 DELETE FROM stage.t_ili2db_dataset WHERE datasetname = 'ch.so.agi.oereb_extract_annex.aktive_gemeinden';
 DELETE FROM stage.t_ili2db_dataset WHERE datasetname = 'ch.so.agi.oereb_extract_annex.katasteramt';
 DELETE FROM stage.t_ili2db_dataset WHERE datasetname = 'ch.so.agi.oereb_extract_annex.stammdaten';
---delete old baskets
-DELETE FROM stage.t_ili2db_basket WHERE attachmentkey LIKE 'ch.so.agi.OeREB_extractAnnex-AktiveGemeinden.xtf-%';
-DELETE FROM stage.t_ili2db_basket WHERE attachmentkey LIKE 'ch.so.agi.OeREB_extractAnnex-KatatasterAmt.xtf-%';
-DELETE FROM stage.t_ili2db_basket WHERE attachmentkey LIKE 'ch.so.agi.OeREB_extractAnnex-Stammdaten.xtf-%';
---DELETE FROM stage.t_ili2db_basket WHERE attachmentkey LIKE 'ch.so.agi.OeREB_extractAnnex.xtf-%'
 --delete everything from stage in the right order
 DELETE FROM stage.oerb_xtnx_v1_0annex_logo;
 DELETE FROM stage.oerb_xtnx_v1_0annex_glossary;
