b0fdf197ba5f012e69d81305da5d2a5488fafd1a
Andreas Schmid <andreas.schmid@bd.so.ch>
2019-12-17 12:11:33 +0100

    Fix grants

--- development_dbs/awjf_statische_waldgrenze_grants.sql
+++ development_dbs/awjf_statische_waldgrenze_grants.sql
@@ -10,8 +10,8 @@ TO public;
 
 -- Grant write privileges
 GRANT SELECT, INSERT, UPDATE, DELETE
-ON ALL TABLES IN SCHEMA awjf_statische_waldgrenzen_oereb
+ON ALL TABLES IN SCHEMA awjf_statische_waldgrenze, awjf_statische_waldgrenzen_oereb
 TO gretl;
 GRANT USAGE
-ON ALL SEQUENCES IN SCHEMA awjf_statische_waldgrenzen_oereb
+ON ALL SEQUENCES IN SCHEMA awjf_statische_waldgrenze, awjf_statische_waldgrenzen_oereb
 TO gretl;
