f02ae49b22e2ea48ddd97c092414308d729a72da
barpasen <peter.senn@bd.so.ch>
2019-12-20 08:24:02 +0100

    Corrected textimweb

--- oereb_grundwasserschutz/insert_oereb_groundwaterprotection_tables.sql
+++ oereb_grundwasserschutz/insert_oereb_groundwaterprotection_tables.sql
@@ -248,8 +248,7 @@ localiseduri AS
         basket_dataset.datasetname,
         0 AS t_seq,
         'de' AS alanguage,
-        CAST('https://geo.so.ch/docs/ch.so.arp.zonenplaene/Zonenplaene_pdf/' || COALESCE(rechtsvorschrften_dokument.textimweb, '404.pdf') AS TEXT) AS atext,
-        --rechtsvorschrften_dokument.textimweb AS atext,
+        rechtsvorschrften_dokument.textimweb AS atext,
         multilingualuri.t_id AS multilingualuri_localisedtext
     FROM
         afu_gewaesserschutz.gwszonen_dokument AS rechtsvorschrften_dokument
