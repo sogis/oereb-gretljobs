b90348a838617728a177e25659bcde49c06dda3d
barpasen <peter.senn@bd.so.ch>
2020-01-08 08:08:21 +0100

    Werte für gwszonen_dokument.textimweb wurden in der Edit-DB geändert

--- oereb_grundwasserschutz/insert_oereb_groundwaterprotection_tables.sql
+++ oereb_grundwasserschutz/insert_oereb_groundwaterprotection_tables.sql
@@ -253,7 +253,11 @@ localiseduri AS
         basket_dataset.datasetname,
         0 AS t_seq,
         'de' AS alanguage,
-        rechtsvorschrften_dokument.textimweb AS atext,
+        CASE
+            WHEN rechtsvorschrften_dokument.textimweb IS NULL
+                THEN 'https://geoweb.so.ch/zonenplaene/Zonenplaene_pdf/404.pdf'
+            ELSE rechtsvorschrften_dokument.textimweb
+        END AS atext,
         multilingualuri.t_id AS multilingualuri_localisedtext
     FROM
         afu_gewaesserschutz.gwszonen_dokument AS rechtsvorschrften_dokument
