2c90455d12f20a4debba7413ce811f62e3404c36
Andreas Neumann <a.neumann@carto.net>
2020-01-23 15:25:07 +0100

    [oereb_av] remove ST_CurveToLine() functions Fixes issue #89

--- oereb_av/select_liegenschaften_lsnachfuehrung.sql
+++ oereb_av/select_liegenschaften_lsnachfuehrung.sql
@@ -1,3 +1,3 @@
 SELECT t_id AS t_id, -1::int8 AS t_basket, 'dm01vch24lv95dliegenschaften_lsnachfuehrung'::varchar(60) AS t_type, NULL::varchar(200) AS t_ili_tid,
-  nbident, identifikator, beschreibung, ST_CurveToLine(perimeter)::geometry(POLYGON,2056) AS perimeter, gueltigkeit, gueltigereintrag, gbeintrag, datum1, datum2
+  nbident, identifikator, beschreibung, perimeter, gueltigkeit, gueltigereintrag, gbeintrag, datum1, datum2
 FROM agi_dm01avso24.liegenschaften_lsnachfuehrung;
