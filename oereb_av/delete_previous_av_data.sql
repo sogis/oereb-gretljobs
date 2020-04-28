f21cfd0a9a0596db34d83bb2f6349f50d7878b02
andreasneumann <a.neumann@carto.net>
2020-04-28 11:44:20 +0200

    [oereb_av] additional tables

--- oereb_av/delete_previous_av_data.sql
+++ oereb_av/delete_previous_av_data.sql
@@ -3,6 +3,7 @@ DELETE FROM ${dbSchema}.dm01vch24lv95dgemeindegrenzen_gemeindegrenze;
 DELETE FROM ${dbSchema}.dm01vch24lv95dgemeindegrenzen_gemeinde;
 DELETE FROM ${dbSchema}.dm01vch24lv95dgemeindegrenzen_gemnachfuehrung;
 -- Liegenschaften
+DELETE FROM ${dbSchema}.dm01vch24lv95dliegenschaften_bergwerk;
 DELETE FROM ${dbSchema}.dm01vch24lv95dliegenschaften_selbstrecht;
 DELETE FROM ${dbSchema}.dm01vch24lv95dliegenschaften_liegenschaft;
 DELETE FROM ${dbSchema}.dm01vch24lv95dliegenschaften_grundstueck;
@@ -12,3 +13,14 @@ DELETE FROM ${dbSchema}.dm01vch24lv95dgebaeudeadressen_gebaeudeeingang;
 DELETE FROM ${dbSchema}.dm01vch24lv95dgebaeudeadressen_lokalisationsname;
 DELETE FROM ${dbSchema}.dm01vch24lv95dgebaeudeadressen_lokalisation;
 DELETE FROM ${dbSchema}.dm01vch24lv95dgebaeudeadressen_gebnachfuehrung;
+-- Bodenbedeckung
+DELETE FROM ${dbSchema}.dm01vch24lv95dbodenbedeckung_boflaeche;
+DELETE FROM ${dbSchema}.dm01vch24lv95dbodenbedeckung_projboflaeche;
+DELETE FROM ${dbSchema}.dm01vch24lv95dbodenbedeckung_gebaeudenummer;
+DELETE FROM ${dbSchema}.dm01vch24lv95dbodenbedeckung_projgebaeudenummer;
+-- Einzelobjekte
+DELETE FROM ${dbSchema}.dm01vch24lv95deinzelobjekte_objektnummer;
+DELETE FROM ${dbSchema}.dm01vch24lv95deinzelobjekte_flaechenelement;
+DELETE FROM ${dbSchema}.dm01vch24lv95deinzelobjekte_einzelobjekt;
+-- Nomenklatur
+DELETE FROM ${dbSchema}.dm01vch24lv95dnomenklatur_flurname;
---
+++ oereb_av/select_bodenbedeckung_projboflaeche.sql
@@ -0,0 +1,3 @@
+SELECT t_id AS t_id, -1::int8 AS t_basket, 'dm01vch24lv95dbodenbedeckung_projboflaeche'::varchar(60) AS t_type, NULL::varchar(200) AS t_ili_tid,
+  geometrie, qualitaet, art, entstehung
+FROM agi_dm01avso24.bodenbedeckung_projboflaeche;
