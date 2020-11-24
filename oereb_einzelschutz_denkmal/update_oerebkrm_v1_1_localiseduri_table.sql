70ae90f384a6410ce913064203d3cd0cb0a86f19
barpasen <peter.senn@bd.so.ch>
2020-11-24 10:12:30 +0100

    requested changes done

--- oereb_einzelschutz_denkmal/update_oerebkrm_v1_1_localiseduri_table.sql
+++ oereb_einzelschutz_denkmal/update_oerebkrm_v1_1_localiseduri_table.sql
@@ -1,5 +1,5 @@
 UPDATE stage.oerebkrm_v1_1_localiseduri
-    SET atext = replace(atext, 'https://ch.so.ada.denkmal', 'https://ch.so.ada.denkmal-stage')
+    SET atext = replace(atext, 'ch.so.ada.denkmal', 'ch.so.ada.denkmal-stage')
 WHERE
     atext LIKE '%ch.so.ada.denkmal%'
 ;
