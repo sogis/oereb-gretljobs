83ad2758d43213ab7994e5709f4ae3e2fa3be0ee
bjsvwpfe <michael.pfeiffer@bd.so.ch>
2020-11-25 11:17:28 +0100

    changed columnname status to astatus due to update plz_ortschaften_pub to ili2pg4

--- oereb_plzo/select_plzortschaft_plz6.sql
+++ oereb_plzo/select_plzortschaft_plz6.sql
@@ -1,3 +1,3 @@
 SELECT t_id AS t_id, -2::int8 AS t_basket, 'plzoch1lv95dplzortschaft_plz6'::varchar(60) AS t_type, NULL::varchar(200) AS t_ili_tid,
-  status AS astatus, inaenderung, plz, zusatzziffern, flaeche, entstehung AS entstehung, plz6_von AS plz6_von
+  astatus, inaenderung, plz, zusatzziffern, flaeche, entstehung AS entstehung, plz6_von AS plz6_von
 FROM agi_plz_ortschaften.plzortschaft_plz6;
