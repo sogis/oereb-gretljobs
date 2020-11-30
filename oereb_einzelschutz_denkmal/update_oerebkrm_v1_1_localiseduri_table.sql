49b86645b214153e31300a6a04708d7f4eced66c
barpasen <peter.senn@bd.so.ch>
2020-11-30 10:44:29 +0100

    Do not change atext on failure of task importDatatToStage

--- oereb_einzelschutz_denkmal/update_oerebkrm_v1_1_localiseduri_table.sql
+++ oereb_einzelschutz_denkmal/update_oerebkrm_v1_1_localiseduri_table.sql
@@ -2,4 +2,6 @@ UPDATE stage.oerebkrm_v1_1_localiseduri
     SET atext = replace(atext, 'ch.so.ada.denkmal', 'ch.so.ada.denkmal-stage')
 WHERE
     atext LIKE '%ch.so.ada.denkmal%'
+AND
+    strpos(atext, '-stage') = 0
 ;
