e7b848bc549fa258a6869dd056cac46da7a6d379
barpasen <38491509+barpasen@users.noreply.github.com>
2021-01-11 08:43:08 +0100

    New theme "WeiteresThema - ch.SO.Einzelschutz - Naturschutzgebiete" (#143)

---
+++ oereb_einzelschutz_naturreservat/delete_oereb_einzelschutz_naturreservat_tables.sql
@@ -0,0 +1,45 @@
+DELETE FROM
+   arp_naturreservate_oereb.transferstruktur_legendeeintrag
+WHERE
+    t_datasetname = 'ch.so.arp.naturreservat'
+;
+DELETE FROM
+   arp_naturreservate_oereb.localiseduri
+WHERE
+    t_datasetname = 'ch.so.arp.naturreservat'
+;
+DELETE FROM
+   arp_naturreservate_oereb.multilingualuri
+WHERE
+    t_datasetname = 'ch.so.arp.naturreservat'
+;
+DELETE FROM
+   arp_naturreservate_oereb.vorschriften_hinweisweiteredokumente
+WHERE
+    t_datasetname = 'ch.so.arp.naturreservat'
+;
+DELETE FROM
+   arp_naturreservate_oereb.transferstruktur_hinweisvorschrift
+WHERE
+    t_datasetname = 'ch.so.arp.naturreservat'
+;
+DELETE FROM
+   arp_naturreservate_oereb.vorschriften_dokument
+WHERE
+    t_datasetname = 'ch.so.arp.naturreservat'
+;
+DELETE FROM
+   arp_naturreservate_oereb.transferstruktur_geometrie
+WHERE
+    t_datasetname = 'ch.so.arp.naturreservat'
+;
+DELETE FROM
+   arp_naturreservate_oereb.transferstruktur_eigentumsbeschraenkung
+WHERE
+    t_datasetname = 'ch.so.arp.naturreservat'
+;
+DELETE FROM
+   arp_naturreservate_oereb.transferstruktur_darstellungsdienst
+WHERE
+    t_datasetname = 'ch.so.arp.naturreservat'
+;
