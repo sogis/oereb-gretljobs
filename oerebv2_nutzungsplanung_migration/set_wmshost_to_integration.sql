11e1799d1cbce1c02c55f8e01d922da8bc54258b
webrian <webrian@gmx.net>
2023-10-19 14:20:44 +0200

    [oerebv2_nutzungsplanung_migration] Remove temp tasks

--- oerebv2_nutzungsplanung_migration/set_wmshost_to_integration.sql
+++
@@ -1,7 +0,0 @@
-UPDATE stage.oerebkrm_v2_0_localiseduri ovl
-SET atext = replace(atext, 'https://geo.so.ch/', 'https://geo-i.so.ch/')
-WHERE ovl.atext ilike '%https://geo.so.ch/%';
-
-UPDATE live.oerebkrm_v2_0_localiseduri ovl
-SET atext = replace(atext, 'https://geo.so.ch/', 'https://geo-i.so.ch/')
-WHERE ovl.atext ilike '%https://geo.so.ch/%';
