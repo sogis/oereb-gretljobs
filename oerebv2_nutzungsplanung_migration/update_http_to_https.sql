0115d0538b39eab97ff39ec33f5f4f41a5d35389
webrian <webrian@gmx.net>
2023-11-02 15:44:28 +0100

    [oerebv2_nutzungsplanung_migration] Set temporarily fake URI

--- oerebv2_nutzungsplanung_migration/update_http_to_https.sql
+++ oerebv2_nutzungsplanung_migration/update_http_to_https.sql
@@ -1,9 +1,9 @@
 -- Update table in stage_v2 schema
 UPDATE stage_v2.oerebkrm_v2_0_localiseduri
-SET atext = replace(atext, 'http://', 'https://')
-WHERE atext ILIKE 'http://geo.so.ch/%';
+SET atext = replace(atext, 'http://geo-fake.so.ch/', 'https://geo.so.ch/')
+WHERE atext ILIKE 'http://geo-fake.so.ch/%';
 
 -- Update table in live_v2 schema
 UPDATE live_v2.oerebkrm_v2_0_localiseduri
-SET atext = replace(atext, 'http://', 'https://')
-WHERE atext ILIKE 'http://geo.so.ch/%';
+SET atext = replace(atext, 'http://geo-fake.so.ch/', 'https://geo.so.ch/')
+WHERE atext ILIKE 'http://geo-fake.so.ch/%';
